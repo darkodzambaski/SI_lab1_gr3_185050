b6bbdb726bc9a8b7f30c9d99408559def5854c48
darkodzambaski <63052308+darkodzambaski@users.noreply.github.com>
2020-04-12 23:14:10 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -5,15 +5,55 @@ class Item {
 
 	//TODO add variable.
 	enum DDV{
-		A=18;
-		B=5;
+		A=0.18;
+		B=0.5;
 		V=0;
 	}
 	//TODO constructor
 
+	public Item(int id, String name, double price,enum DDV) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.DDV = DDV;
+	}
+
 	//TODO setters and getters
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public enum getDDV(){
+		return DDV;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 	double taxReturn () {
 		//TODO
+		float r=0.15;
+		float dd=0;
+		float kd=0;
+		dd=price*getDDV;
+		kd=dd*r;
+		return taxReturn();
 	}
 }
